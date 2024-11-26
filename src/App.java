44b5d2edb90f2ba1a222d41afd7c190fff19dbbe
FranciscoABruno <usr3760@salesianos-lacuesta.net>
2024-11-26 12:43:38 +0000

    experimento uno

--- src/App.java
+++ src/App.java
@@ -7,28 +7,17 @@ public class App {
         Store store = new Store();
 
         Farmer paco = new Farmer(store, "Paco", 5);
-        Farmer ramon = new Farmer(store, "Ramón", 5);
 
-        Customer faustino = new Customer(store, "Faustino", 2);
-        Customer gentleman = new Customer(store, "Mr. Gentleman", 3);
-        Customer loquendo = new Customer(store, "Loquendo", 5);
+        Customer faustino = new Customer(store, "Faustino", 5);
 
         paco.start();
-        ramon.start();
         faustino.start();
-        gentleman.start();
-        loquendo.start();
         
         try {
             paco.join();
-            ramon.join();
             faustino.join(1000);
-            gentleman.join(2000);
-            loquendo.join(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        System.out.println("Simulation complete!");
     }
 }
